7650aef085cccb00165fc16f6389ce4e6e6a7b2c
Tim Goens <46302093+TimothyCGoens@users.noreply.github.com>
2019-08-08 21:44:22 -0500

    Add files via upload

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tim Goens on 8/8/19.
+//  Copyright © 2019 Tim Goens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
