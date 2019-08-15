6624a93eab4db4cc819be474cb31e484226d7c2e
Tim Goens <46302093+TimothyCGoens@users.noreply.github.com>
2019-08-14 21:49:04 -0500

    Add files via upload

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Tim Goens on 8/9/19.
+//  Copyright © 2019 Tim Goens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newBallImage()
+        
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+}
+
