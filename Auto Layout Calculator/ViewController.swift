0a654af4e27a274539c2f59174c28adb38379b7d
Tim Goens <46302093+TimothyCGoens@users.noreply.github.com>
2019-08-17 21:16:10 -0500

    Add files via upload

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Tim Goens on 8/15/19.
+//  Copyright © 2019 Tim Goens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
